d9534abe79e4a0021a64e90700a8f8b4be0dcd4d
Cami Fischmann <fischmannc24@mail.wlu.edu>
2023-10-09 16:26:29 -0400

    slight format change

--- app.R
+++ app.R
@@ -8,9 +8,7 @@ ui <- fluidPage(
               "Airports", 
               choices = unique(flights$origin)),
   sliderInput("xAxisRange", "Range of Delays", 0, max(flights$dep_time, na.rm = TRUE), 500),
-  plotOutput("departureDelayPlot")
-  
-)
+  plotOutput("departureDelayPlot"))
 #titlePanel gives app title; use ?selectInput and ?selectOutput in console for help figuring out what to code in these lines
 #done with ui; ui sets up where stuff is on page with relation to one another
 
