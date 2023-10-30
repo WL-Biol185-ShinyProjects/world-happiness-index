af718989c48d3a3bf105febe2151d4fb61351336
Cami Fischmann <fischmannc24@mail.wlu.edu>
2023-10-30 15:42:05 -0400

    Intro Page folder

---
+++ Intro Page/server.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+whrDATA <- read.csv("whrDATA.csv")
+
+server <- function(input, output) {
+  output$HappinessvsCountryPlot <- renderPlot({
+    whrDATA %>%
+      filter(Year == input$selectYear) %>%
+      ggplot(aes(Country, Happiness)) + geom_bar(stat = "identity", position = position_dodge(0.92), alpha = 0.5) +
+      labs(title = "Happiness by Country", x = "Country", y = "Happiness Index Value") +
+      scale_x_discrete(guide = guide_axis(angle = 90)) +
+      theme_classic()
+  })
+}
