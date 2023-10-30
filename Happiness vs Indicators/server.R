65e603ffaf54f201ab2ae316c4f1e813825f2188
emily <sansburye25@mail.wlu.edu>
2023-10-30 15:43:04 -0400

    Happiness vs Indicators folder

---
+++ Happiness vs Indicators/server.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(shiny)
+
+whrDATA <- read.csv("whrDATA.csv")
+
+server <- function(input, output) {
+  
+  output$HappinessvsGDP <- renderPlot ({
+    if (input$selectX == "GDP") {
+        whrDATA %>%
+          filter(Year == input$selectyear) %>%
+            ggplot(aes(GDP, Happiness)) + geom_point(color ="blue") + ggtitle("Happiness vs GDP")
+      }
+    
+    else if (input$selectX == "Support") {
+      whrDATA %>%
+        filter(Year == input$selectyear) %>%
+        ggplot(aes(Support, Happiness)) + geom_point(color ="blue") + ggtitle("Happiness vs Support")
+     }
+      else if (input$selectX == "LE") {
+        whrDATA %>%
+          filter(Year == input$selectyear) %>%
+          ggplot(aes(LE, Happiness)) + geom_point(color = "blue" ) + ggtitle("Happiness vs LE")
+     }
+    
+    else if (input$selectX == "Freedom") {
+      whrDATA %>%
+        filter(Year == input$selectyear) %>%
+        ggplot(aes(Freedom, Happiness)) + geom_point(color = "blue") + ggtitle("Happiness vs Freedom")
+    }
+  })
+  
+  output$plot_brushinfo <- renderTable({
+    brush <- input$plot_brush
+    brushedPoints(whrDATA, brush) %>%
+      filter(Year == input$selectyear)
+    
+   
+    
+  })
+}
+
+  
+
---
+++ Happiness vs Indicators/ui.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+whrDATA <- read.csv("whrDATA.csv")
+
+fluidPage(
+  titlePanel("Happiness vs Different Indicators"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("selectyear", "Year:",
+              choices=unique(whrDATA$Year)),
+  hr(),
+  helpText("Available Years within the Dataset"),
+  selectInput("selectX", "X:",
+              choices=colnames(select(whrDATA, 4:7)))
+  ),
+  
+  mainPanel (
+  p("Please explore this interactive graph to discover the relationship between a country's happiness and different indicators. 
+             You can alter the specific x-axis indicator to see how this impacts a country's happiness level as well as choose a specific year of interest. 
+             Hovering over each point will display the corresponding country.", style = 'times'),
+             plotOutput("HappinessvsGDP",
+              brush = brushOpts(
+                id = "plot_brush",
+                        )), 
+  tableOutput("plot_brushinfo"),
+
+
+
+)
+)
+)
+
+  
