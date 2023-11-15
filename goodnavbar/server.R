a61ba533092ea75b7322ef9568717a7e77f96480
emily <sansburye25@mail.wlu.edu>
2023-11-15 15:04:22 -0500

    putting dataframe in server

--- goodnavbar/server.R
+++ goodnavbar/server.R
@@ -9,6 +9,25 @@ library(DT)
 library(shinyWidgets)
 library(RColorBrewer)
 
+world <- read.csv("world-happiness-report.csv")
+
+geo <- geojson_read("countries.geo.json", what = "sp")
+
+whrDATA <- world %>%
+  select(1:7) %>%
+  rename(Country = Country.name,
+         Year = year,
+         Happiness = Life.Ladder,
+         GDP = Log.GDP.per.capita,
+         Support = Social.support,
+         LE = Healthy.life.expectancy.at.birth,
+         Freedom = Freedom.to.make.life.choices)
+
+whr2020 <- whrDATA %>%
+  filter(Year == 2020) %>%
+  filter(!is.na(GDP)) %>%
+  filter(!is.na(LE)) %>%
+  filter(!is.na(Freedom))
 
 function(input, output, session) {
 
