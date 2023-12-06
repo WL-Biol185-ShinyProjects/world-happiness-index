e3117fde42038daae2f640df988bd3d817860964
Cami Fischmann <fischmannc24@mail.wlu.edu>
2023-12-06 16:52:36 -0500

    Drop-down menu for regions in regression tab/addition of regression datasets

--- server.R
+++ server.R
@@ -119,6 +119,37 @@ regDATA <- read.csv("overtime.csv") %>%
                values_to = 'RegCoef') %>% 
   arrange(Year, Predictor, RegCoef)
 
+
+regASIA <- read.csv("asia.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+
+regEUROPE <- read.csv("europe.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+regAFRICA <- read.csv("africa.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+
+regAMERICAS <- read.csv("americas.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
 lnglat <- read.csv("country-capital-lat-long-population.csv") %>%
   select(Country, Longitude, Latitude)
 
@@ -402,12 +433,48 @@ function(input, output, session) {
   
   
   output$WWRegressionPlot <- renderPlot({
-    regDATA %>%
-      ggplot(aes(x = Year, y = RegCoef, group = Predictor)) +
-      geom_line(aes(color = Predictor)) +
-      geom_point(aes(color = Predictor)) +
-      labs(title = "Correlation of Predictors and Happiness Over Time", x = "Year", y = "Regression Coefficient")
+    if (input$selectRegion == "Worldwide") {
+      regDATA %>%
+        ggplot(aes(x = Year, y = RegCoef, group = Predictor)) +
+        geom_line(aes(color = Predictor)) +
+        geom_point(aes(color = Predictor)) +
+        labs(title = "Correlation of Predictors and Happiness Over Time (Worldwide)", x = "Year", y = "Regression Coefficient")
+    }
+    
+    else if (input$selectRegion == "Asia") {
+      regASIA %>%
+        ggplot(aes(x = Year, y = RegCoef, group = Predictor)) +
+        geom_line(aes(color = Predictor)) +
+        geom_point(aes(color = Predictor)) +
+        labs(title = "Correlation of Predictors and Happiness Over Time (Asia)", x = "Year", y = "Regression Coefficient")
+    }
+    
+    else if (input$selectRegion == "Europe") {
+      regEUROPE %>%
+        ggplot(aes(x = Year, y = RegCoef, group = Predictor)) +
+        geom_line(aes(color = Predictor)) +
+        geom_point(aes(color = Predictor)) +
+        labs(title = "Correlation of Predictors and Happiness Over Time (Europe)", x = "Year", y = "Regression Coefficient")
+    }
+    
+    else if (input$selectRegion == "Africa") {
+      regAFRICA %>%
+        ggplot(aes(x = Year, y = RegCoef, group = Predictor)) +
+        geom_line(aes(color = Predictor)) +
+        geom_point(aes(color = Predictor)) +
+        labs(title = "Correlation of Predictors and Happiness Over Time (Africa)", x = "Year", y = "Regression Coefficient")
+    }
+    
+    else if (input$selectRegion == "Americas") {
+      regAMERICAS %>%
+        ggplot(aes(x = Year, y = RegCoef, group = Predictor)) +
+        geom_line(aes(color = Predictor)) +
+        geom_point(aes(color = Predictor)) +
+        labs(title = "Correlation of Predictors and Happiness Over Time (Americas)", x = "Year", y = "Regression Coefficient")
+    }
+    
   })
-  }
+}
+
   
 
--- ui.R
+++ ui.R
@@ -119,6 +119,37 @@ regDATA <- read.csv("overtime.csv") %>%
                values_to = 'RegCoef') %>% 
   arrange(Year, Predictor, RegCoef)
 
+
+regASIA <- read.csv("asia.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+
+regEUROPE <- read.csv("europe.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+regAFRICA <- read.csv("africa.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+
+regAMERICAS <- read.csv("americas.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
 lnglat <- read.csv("country-capital-lat-long-population.csv") %>%
   select(Country, Longitude, Latitude)
 
@@ -308,7 +339,16 @@ navbarPage("World Happiness Report",
                                 The resulting regression coefficients indicate how strongly each of the predictor variables correspond with the Happiness outcome.
                                 Larger regression coefficients indicate that the predictor variable had a greater influence on the Happiness score.
                                 Negative regression coefficients suggest that the predictor is negatively correlated with Happiness -- e.g., as the predictor increases, Happiness decreases."),
-                              plotOutput("WWRegressionPlot", height = ("80vh")))),
+                              fluidRow(
+                                selectInput("selectRegion",
+                                            "Region",
+                                            choices = c("Worldwide",
+                                                        "Africa",
+                                                        "Americas",
+                                                        "Asia",
+                                                        "Europe"), 
+                                            width = "50%"),
+                              plotOutput("WWRegressionPlot", height = ("80vh"))))),
            
            
            tabPanel("About",
