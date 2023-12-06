6422a199b80d10508ec940559a1469cdbd8f6343
Zainab Madan <madanz25@mail.wlu.edu>
2023-12-06 17:53:27 -0500

    slider changes but please ignore

--- server.R
+++ server.R
@@ -25,7 +25,6 @@ whrDATA <- world %>%
          Freedom = Freedom.to.make.life.choices) %>%
   arrange(Year, Country, Happiness, GDP, Support, LE, Freedom) 
 
-
 whr2020 <- whrDATA %>%
   filter(Year == 2020) %>%
   filter(!is.na(GDP)) %>%
@@ -156,7 +155,6 @@ flag[185, 1] <- "United Arab Emirates"
 flag[186, 1] <- "United Kingdom"
 flag[187, 1] <- "United States"
 
-
 worldflag <- left_join(llworld, flag)
 
 llworld <- worldflag
@@ -172,6 +170,7 @@ newcol <- paste0("<p><b><em>Country</b></em>", "=", llworld$Country,
 newcol <- as.data.frame(newcol)
 
 llworld2 <- bind_cols(llworld, newcol)
+
 
 function(input, output, session) {
 
