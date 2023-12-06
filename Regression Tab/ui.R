a1601bc3b5145920c550ca748826aef18ab1a5f6
Cami Fischmann <fischmannc24@mail.wlu.edu>
2023-12-06 15:22:59 -0500

    Updated regression stuff

--- Regression Tab/ui.R
+++ Regression Tab/ui.R
@@ -10,6 +10,38 @@ regDATA <- read.csv("overtime.csv") %>%
                values_to = 'RegCoef') %>% 
   arrange(Year, Predictor, RegCoef)
 
+
+
+regASIA <- read.csv("asia.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+
+regEUROPE <- read.csv("europe.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+regAFRICA <- read.csv("africa.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
+
+regAMERICAS <- read.csv("americas.csv") %>%
+  pivot_longer(cols = !(c(Year)),
+               names_to = 'Predictor',
+               values_to = 'RegCoef') %>% 
+  arrange(Year, Predictor, RegCoef)
+
+
 ui <- fluidPage(
   titlePanel("Strength of Variables in Predicting Happiness Over Time"),
   setBackgroundColor("aliceblue"),
@@ -20,8 +52,18 @@ ui <- fluidPage(
               The resulting regression coefficients indicate how strongly each of the predictor variables correspond with the Happiness outcome.
               Larger regression coefficients indicate that the preictor variable had a greater influence on the Happiness score.
               Negative regression coefficients suggest that the predictor is negatively correlated with Happiness -- e.g., as the predictor increases, Happiness decreases."),
+            
+            fluidRow(
+              selectInput("selectRegion",
+                          "Region",
+                          choices = c("Worldwide",
+                                      "Africa",
+                                      "Americas",
+                                      "Asia",
+                                      "Europe"), 
+                          width = "50%"),
               
-  plotOutput("WWRegressionPlot")))
+  plotOutput("RegressionPlots"))))
 
 
 
